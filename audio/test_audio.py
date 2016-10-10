5b0bb66eff34d97defce96949de9da35944d7dff
Alexandra Brown <alexbro@umich.edu>
2016-10-10 17:17:42 -0400

    add test_audio.py

---
+++ audio/test_audio.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#import sublime
+#import sublime_plugin
+
+#Test plugin to explore speech options in python
+
+from os import system
+
+function = ' foo. ';
+params = ' no inputs. ';
+returns = ' int. '
+
+system('say function ' + function + params + 'returns a ' + returns)
