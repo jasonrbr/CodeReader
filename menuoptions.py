a342a04c7764f7ba75a80d7e7b3f17d612c06a0b
Madeline Andrew <madclare@umich.edu>
2016-10-12 14:51:16 -0400

    var_types includes void

--- menuoptions.py
+++ menuoptions.py
@@ -7,8 +7,9 @@ from collections import deque
 # function, class level scopes
 # not loops
 
+# TODO only works for built in types
 type_patt = re.compile('(string|int|double|bool)') #TODO remove
-var_types = r'(string|int|double|bool)'
+var_types = r'(string|int|double|bool|void)'
 variable_patt = re.compile(var_types+r'\s([\w]+)\s?\=?\s?([\w]+)?;')
 function_patt = re.compile(var_types+r'\s([\w]+)\((.*)\)')
 class_patt = re.compile(r'(class|struct)\s([\w]+)\s?\{')
@@ -27,32 +28,52 @@ class Scope():
 	def __str__(self):
 		# return ' '.join([self.return_type, self.name, "variables({})".format(str(len(self.variables))), "children({})".format(str(self.children)), 'extras({})'.format(len(self.extras))])
 		return ' '.join([self.return_type, self.name])
-# return root node to the hierarchy
-# assumes only 1 bracket per line at the moment
-# assumes you put open bracket on the same line as function declaration
-# file is list of strings of content of file
-def GetHierarchy(content):
-	global_scope = Scope(name_in="Global", return_type_in="NONE")
-	count = 0
+
+	# Returns region of child, returns None
+	# if child doesn't exist yo
+	#def get_child()
+
+""" return root node to the hierarchy
+ 	assumes only 1 bracket per line at the moment
+ 	assumes you put open bracket on the same line as function declaration
+ 	file is list of strings of content of file """
+def GetHierarchy(view, region, name, return_type=None):
+	# TODO: don't pass in name or return type? :/
+	scope = Scope(name_in=name, return_type_in=return_type)
+
+	idx = region.begin()
+	#while idx < view.size():
+	# TODO: make work for classes, girl
+	child_declaration = view.find(var_types+r'\s([\w]+)\((.*)\)', idx)
+	print(child_declaration)
+	idx = child_declaration.end() + 1
+	print(idx)
+
+	child_declaration = view.find(var_types+r'\s([\w]+)\((.*)\)', idx)
+	print(child_declaration)
+	idx = child_declaration.end() + 1
+	print(idx)
+
+	"""#count = 0
 	for idx in range(0, len(content)):
 		line = content[idx]
 		if function_patt.match(line):
-			count += 1
+			#count += 1
 			return_type = function_patt.match(line).group(1)
 			name = function_patt.match(line).group(2)
 			child, idx = GetFunctionDefinition(name, return_type, content[idx+1:])
 
-			global_scope.children.append(child)
+			scope.children.append(child)
 		elif variable_patt.match(line):
 			v = Variable(getType(line), getName(line), getValue(line))
-			global_scope.variables.append(v)
+			scope.variables.append(v)
 		else:
-			global_scope.extras += line
-			print(line)
+			scope.extras += line
+			#print(line)
 
 
 	# TODO test end line num?
-	return global_scope, count
+	return scope, count"""
 
 class Variable():
 	def __init__(self, type_in, name_in, value_in):
@@ -75,8 +96,8 @@ def getValue(line):
 def GetFunctionDefinition(name, return_type, content):
 
 	func_scope = Scope(name_in=name, return_type_in=return_type)
-
-	print('defining func', name)
+"""
+	#print('defining func', name)
 	for idx in range(0, len(content)):
 		line = content[idx]
 		if function_patt.match(line):
@@ -92,32 +113,40 @@ def GetFunctionDefinition(name, return_type, content):
 			return func_scope, idx+1
 		else:
 			func_scope.extras += line
-			print('skipped', line)
+			#print('skipped', line)
 
 
-	print("Function never ended. Terminating.")
-	exit(1)
+	#print("Function never ended. Terminating.")
+	exit(1)"""
 
-def printHierarchy(head):
-	q = deque()
-	q.appendleft(head)
+"""def printHierarchy(head):
+	print("Hello World")
+	#q = deque()
+	#q.appendleft(head)
 
-	while q:
-		cur_node = q.pop()
+	#i = 0
+	#while i < 100:
+	#	print(i)
+	#	cur_node = q.pop()
 		# print(str(cur_node))
-		for child in cur_node.children:
-			q.appendleft(child)
+	#	for child in cur_node.children:
+	#		q.appendleft(child)"""
 
+# TODO: only works for single file
 class MenuOptionsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# self.view.insert(edit, 0, "Hello, World!")
-		txt = self.view.substr(sublime.Region(0, self.view.size()))
-		head, count = GetHierarchy(txt.split('\n'))
+		file_start = 0
+		file_end = self.view.size()
+
+		#src_code = self.view.split_by_newlines(sublime.Region(file_start, file_end))
+		src_code = sublime.Region(file_start, file_end)
+		head = GetHierarchy(view=self.view, region=src_code, name="Global")
 		# head, end_idx = GetHierarchy(txt.split('\n'))
 		# if end_idx != self.view.size():
 		# 	print('end_idx={}, size={}'.format(end_idx, self.view.size()))
 		# printHierarchy(head)
-		print (count)
-		print('done')
+		#printHierarchy(head)
+		#print ("Count: {}".format(num_scopes_found))
+		#print('done')
 		# return head
 
