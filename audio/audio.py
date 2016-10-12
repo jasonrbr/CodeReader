e17facb0c1b3fe253aad1847aa313692d04e6ded
Alexandra Brown <alexbro@umich.edu>
2016-10-12 17:50:49 -0400

    Completed audio function for Mac and Windows OS

--- audio/audio.py
+++ audio/audio.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
-import os
 import sys
 
 # Perform text to speech on input with the corresponding modules based on the operating system
 def say(input):
 	if (sys.platform == 'darwin'):
+		import os
 		os.system('say ' + input)
-	elif(sys.platform == 'win32'):
-		print('Windows!')
-	elif(sys.platform == 'linux2'):
-		print('Linux!')
+
+	elif (sys.platform == 'win32'):
+		# Need to install pywin32 for this to work
+		# https://sourceforge.net/projects/pywin32/files/pywin32/Build%20220/
+		# E.g., pywin32-220.win-amd64-py3.4.exe if you are running python 3.4
+		import win32com.client
+		speaker = win32com.client.Dispatch("SAPI.SpVoice")
+		speaker.Speak(input)
+
 	else:
 		print('OS not supported')
-
-
-say('Hello world')
-
-function = ' foo. ';
-params = ' no inputs. ';
-returns = ' int. '
-say ('function ' + function + params + 'returns a ' + returns)
