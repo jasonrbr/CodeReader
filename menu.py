d2b8907d4fb288de7c8f7deaca6ce2e3305aeddc
Madeline Andrew <madclare@umich.edu>
2016-10-15 22:20:25 -0500

    comments

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import sublime
 import sublime_plugin
 from .scopes import *
-# TODO: use pypeg
 
+# TODO: use pypeg
 class MenuNode():
 	def __init__(self, view, scope, parent=None):
 		self._view = view
@@ -17,11 +17,20 @@ class MenuNode():
 		if self._scope.type != func_type:
 			self._generate_children()	
 
-	def get_children(self, key=None):
-		if key and key in self._children and len(self._children[key]):
-			return self._children[key]
+	# @param: child_type: when child_type == None, returns a list
+	#					  of available child_types. If the node doesn't
+	#					  have a certain child_type, it's not included
+	#					  in the list. Child types include func_type
+	#					  class_type, and other_type
+	#
+	#					  when child_type != None, returns a list of
+	#					  all children with that available type. If none
+	#					  exist, returns None
+	def get_children(self, child_type=None):
+		if child_type and child_type in self._children and len(self._children[child_type]):
+			return self._children[child_type]
 
-		if not key:
+		if not child_type:
 			children = list()
 			for key, value in self._children.items():
 				if len(value):
@@ -44,16 +53,17 @@ class MenuNode():
 		return self._scope
 	
 	# TODO: use pypeg
-	# TODO: only works when '{' on same line
-	# TODO: list of pairs instead of list of dicts
-	# TODO: stop at end of region
+	# TODO: only works when '{' on same line as declaration
+	#
+	# Stores all children found within the body of this
+	# MenuNode's scope
 	def _generate_children(self):
 		idx = self._scope.body.begin()
 		while True:
 			start_region = \
 				self._view.find('{', idx)
 
-			# All children have been found
+			# All available children have been found
 			if not start_region:
 				return
 
@@ -63,7 +73,8 @@ class MenuNode():
 
 			child = self._get_child(start_region)	
 
-			# End bracket not found
+			# A child is missing a closing bracket
+			# TODO: error message?
 			if not child:
 				return
 
@@ -71,14 +82,46 @@ class MenuNode():
 
 			idx = child.body.end() + 1
 
-	# Assumes all lines contain and end with a single ';'
+	# Find and returns a child of this MenuNode's scope
+	#	@param: start_region: region containing the declaration of
+	#			the child
+	def _get_child(self, start_region):
+		# Get child's declaration
+		declaration = self._view.substr(self._view.line(start_region))
+
+		# Get child's definition (body)
+		body_start_row = self._view.rowcol(start_region.begin())[0] + 1
+		body_start_point = self._view.text_point(body_start_row, 0)
+		body = self._get_child_body(body_start_point, self._scope._body.end())
+
+		# Child has no closing bracket
+		if not body:
+			return None
+
+		# TODO: other :p
+		if 'class' in declaration or 'struct' in declaration:
+			return Class(self._view, body, declaration)
+		else:
+			return Function(self._view, body, declaration)
+
+	# TODO: use pypeg
+	# NOTE: Assumes all lines contain and end with a single ';'
+	#
+	# Finds and returns the Region containing the child's definition (body)
+	# 	@param: start_point: point located in the first line of the child body
+	#	@param: end_point: point at end of the MenuNode's scope. If final '}' is
+	#			not found once this point is reached, the child has no closing
+	# 			bracket (compile error) and 'None' is returned
 	def _get_child_body(self, start_point, end_point):	
 		body_regions = \
 			self._view.split_by_newlines(
 				sublime.Region(start_point, end_point))
 
+		# Tracks the number of open/closed brackets
+		# respectively
 		open_count = 1
 		closed_count = 0
+		
 		end_point = None
 
 		for region in body_regions:
@@ -93,31 +136,13 @@ class MenuNode():
 			if open_count == closed_count:
 				break
 		
-		# Missing closing bracket
+		# Child has no closing bracket
 		if open_count != closed_count:
 			return None
 
 		return sublime.Region(start_point, end_point)
 
-	def _get_child(self, start_region):
-		# Get declaration
-		declaration = self._view.substr(self._view.line(start_region))
-
-		# Get body
-		body_start_row = self._view.rowcol(start_region.begin())[0] + 1
-		body_start_point = self._view.text_point(body_start_row, 0)
-		body = self._get_child_body(body_start_point, self._scope._body.end())
-
-		# body not found
-		if not body:
-			return None
-
-		# TODO: other
-		if 'class' in declaration or 'struct' in declaration:
-			return Class(self._view, body, declaration)
-		else:
-			return Function(self._view, body, declaration)
-
+# Test
 class MenuCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		file_start = 0
