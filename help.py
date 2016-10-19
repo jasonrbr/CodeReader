6d399165d693e8faa06969531585d4e90707a9a4
Alexandra Brown <alexbro@umich.edu>
2016-10-18 20:52:49 -0400

    make the help_text reading less dumb

--- help.py
+++ help.py
@@ -1,10 +1,12 @@
 import sublime
 import sublime_plugin
+import os
 from .audio import say
-
-help_text = '''This is the help text. Use the arrow keys to cycle through the available functions or classes. Press enter to make a selection. Command: _____ Toggle line numbers on or off. Use the arrow keys to read through a function. End of the help document.'''
 
 # A key binding will activate this function to read aloud the help menu located in help.txt
 class HelpMeCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		say(help_text)
+    def run(self, edit):
+        fn = os.path.join(os.path.dirname(__file__), 'help.txt')
+        with open(fn, 'r') as myfile:
+            data = myfile.read().replace('\n', '')
+            say(data)
