1004b231bbc77710ede04fa0371a5f17efba30de
Madeline Andrew <madclare@umich.edu>
2016-11-08 22:45:08 -0500

    Revert "Scoped panels"

--- test_cpp.py
+++ test_cpp.py
@@ -3,14 +3,12 @@ from antlr4 import *
 from MyGrammarLexer import MyGrammarLexer
 from MyGrammarParser import MyGrammarParser
 
-
 def main(argv):
     input = FileStream(argv[1])
     lexer = MyGrammarLexer(input)
     stream = CommonTokenStream(lexer)
     parser = MyGrammarParser(stream)
     tree = parser.StartRule()
-
 
 if __name__ == '__main__':
     main(sys.argv)
