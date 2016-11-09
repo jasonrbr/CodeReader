85c6b3bb0b54d44bf394ffa945339d59ef7682e5
Jason Brown <jasonrbr@umich.edu>
2016-11-09 01:05:18 -0500

    some anaconda refactoring and add equality and params to scopes

--- codereader.py
+++ codereader.py
@@ -9,18 +9,20 @@ from .audio import say
 global_namespace = 'global namespace'
 
 # Menu option Strings:
-go_up = 'go up to scope, ' # Concat with scope you're currently in
-read = 'read ' # Concat with scope name
+go_up = 'go up to scope, '  # Concat with scope you're currently in
+read = 'read '  # Concat with scope name
 global_name = 'global'
 
+
 class CodeReaderCommand(sublime_plugin.TextCommand):
-    def run(self, edit):        
+    def run(self, edit):
         file_start = 0
         file_end = self.view.size()
         src_code = sublime.Region(file_start, file_end)
-            
+
         # Set root to Global Namespace node
-        global_scope = Scope(view=self.view, body=src_code, name=global_namespace)
+        global_scope = Scope(
+            view=self.view, body=src_code, name=global_namespace)
         self._node = MenuNode(view=self.view, scope=global_scope)
 
         self._show_options_menu()
@@ -30,39 +32,40 @@ class CodeReaderCommand(sublime_plugin.TextCommand):
     #   @param: on_done: callback function
     #   @param: on_highlighted: callback function
     def _show_panel(self, options, on_done, on_highlight):
-        sublime.active_window().show_quick_panel(options, 
-                                                 on_done, 
+        sublime.active_window().show_quick_panel(options,
+                                                 on_done,
                                                  on_highlight=on_highlight)
 
     # Displays a panel containing the current node's
     # children of the specified type.
-    #   @param: child_type: one of func_type, class_type, 
+    #   @param: child_type: one of func_type, class_type,
     #                       or other_type
     def _show_children_menu(self, child_type):
         self._panel_options = list()
-        self._panel_options.append("Scope {}s {}".format(self._node.name, 
-                                                  child_type))
+        self._panel_options.append("Scope {}s {}".format(self._node.name,
+                                                         child_type))
 
         children = self._node.get_children(child_type)
 
         # If node has no children, only display "return to options menu"
         # option. TODO: this shouldn't happen possibly assert false here
         if not children:
-            self._show_panel(self._panel_options, 
-                             self._on_children_done, 
+            self._show_panel(self._panel_options,
+                             self._on_children_done,
                              self._on_highlight_done)
             return
 
-        self._children_options = dict() 
+        self._children_options = dict()
 
         for child in children:
             self._panel_options.append(child.name)
             self._children_options[child.name] = child
 
-        self._panel_options.append("See all children options for {}".format(self._node.name))
+        self._panel_options.append(
+            "See all children options for {}".format(self._node.name))
 
-        self._show_panel(self._panel_options, 
-                         self._on_children_done, 
+        self._show_panel(self._panel_options,
+                         self._on_children_done,
                          self._on_highlight_done)
 
     # Displays a panel containing the current node's
@@ -79,16 +82,17 @@ class CodeReaderCommand(sublime_plugin.TextCommand):
 
         # Add read_scope option if the current scope is a
         # function or a class
-        if self._node.scope.type == func_type or \
-            self._node.scope.type == class_type:
-                self._panel_options.append(read + self._node.name)
+        if (self._node.scope.type == func_type or
+                self._node.scope.type == class_type):
+            self._panel_options.append(read + self._node.name)
 
         # Add go_up hierarchy option if the current scope
         # is not the global scope
         if self._node.parent:
             self._panel_options.append(go_up + self._node.parent.name)
 
-        self._show_panel(self._panel_options, self._on_options_done, self._on_highlight_done)
+        self._show_panel(self._panel_options,
+                         self._on_options_done, self._on_highlight_done)
 
     def _on_options_done(self, ind):
         # show_quick_panel() calls its callback with -1
@@ -122,11 +126,11 @@ class CodeReaderCommand(sublime_plugin.TextCommand):
         # if the user cancels the panel
         if(ind == -1):
             return
-        
+
         # The first item in the panel is the panel title
         if(ind == 0):
             self._show_options_menu()
-       
+
         end_ind = len(self._panel_options) - 1
         if(ind == end_ind):
             self._show_options_menu()
@@ -135,12 +139,12 @@ class CodeReaderCommand(sublime_plugin.TextCommand):
 
         child = self._children_options[selection]
 
-        self._node = MenuNode(view=self.view, 
-                              scope=child, 
+        self._node = MenuNode(view=self.view,
+                              scope=child,
                               parent=copy.deepcopy(self._node))
 
         self._show_options_menu()
 
     def _on_highlight_done(self, ind):
-        #say menu options when option is highlighted
+        # say menu options when option is highlighted
         say(self._panel_options[ind])
--- scopes.py
+++ scopes.py
@@ -2,21 +2,21 @@ import sublime_plugin
 import sublime
 
 # Scope types
-func_type = 'functions'
-class_type = 'classes'
-other_type = 'other'
+func_scope_type = 'functions'
+class_scope_type = 'classes'
+other_scope_type = 'other'
 
 
 class Scope():
-    def __init__(self, view, body, name, s_type=None):
+    def __init__(self, view, body, name, scope_type=None):
         self._view = view
-        self._type = s_type
+        self._scope_type = scope_type
         self._body = body
         self._name = name
 
     @property
     def type(self):
-        return self._type
+        return self._scope_type
 
     @property
     def body(self):
@@ -31,13 +31,19 @@ class Function(Scope):
     def __init__(self, view, body, declaration):
         super().__init__(view, body,
                          declaration.split()[1].split('(')[0],
-                         func_type)
+                         func_scope_type)
 
         self._returns = declaration.split()[0]
+        self._params = declaration.split('(')[1].split(')')[0].split(',')
+        self._params = [s.strip() for s in self._params]  # trim whitespace
 
     @property
     def declaration(self):
         return "function {} returns {}".format(self._name, self._returns)
+
+    @property
+    def params(self):
+        return "takes {}".format(', '.join(self._params))
 
     def __str__(self):
         func_str = self.declaration + '\n'
@@ -56,11 +62,15 @@ class Function(Scope):
 
         return func_str
 
+    def __eq__(self, other):
+        return (self.declaration == other.declaration and
+                self._params == other._params)
+
 
 class Class(Scope):
     def __init__(self, view, body, declaration):
         super().__init__(view, body,
-                         declaration.split()[1], class_type)
+                         declaration.split()[1], class_scope_type)
 
     @property
     def declaration(self):
@@ -83,9 +93,26 @@ class Class(Scope):
 
         return class_str
 
+    def __eq__(self, other):
+        return self.declaration == other.declaration
+
 
 # Test
 class ScopeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        func = Function(self.view, sublime.Region(82, 122), "int main() {")
-        print(str(func))
+        func = Function(self.view, sublime.Region(31, 54),
+                        "int foo(int x, int y, int z) {")
+        func2 = Function(self.view, sublime.Region(31, 54),
+                         "int foo(int x, int y, int z) {")
+        func3 = Function(self.view, sublime.Region(80, 95),
+                         "int foo(int x, int y) {")
+        print(str(func3))
+        print(func3.params)
+        if func == func2:
+            print('equal')
+        if func != func3:
+            print('yay')
+        if func != func2:
+            print('boo')
+        if func == func3:
+            print('boo you whore')
