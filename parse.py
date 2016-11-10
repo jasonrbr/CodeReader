57685f4e939f48bdb510c60b9690dac35026b4ca
Madeline Andrew <madclare@umich.edu>
2016-11-10 03:39:39 -0500

    parse module parses scopes

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 import sublime
 import sublime_plugin
-
+from .scopes import Function, Class
 
 def get_decl_start(view, symbol_start):
     """
@@ -31,35 +31,48 @@ def get_decl_start(view, symbol_start):
 
     return start_decl
 
+def get_decl_end(view, symbol_end):
+    end_decl = symbol_end
 
-def get_scope_end(view, symbol_end):
-    """
-    Returns the end point of the symbol's definition
-    in the view.
-
-    Parameters:
-        view - sublime view
-        symbol_end - the end point of the symbol
-                     withing the view
-    """
-    end_point = symbol_end
-
-    # Find first open bracket or ';'
+     # Find first open bracket or ';'
     while True:
-        if end_point == view.size() - 1:
+        if end_decl == view.size() - 1:
             print("Error: missing open bracket or semicolon")
             assert False
 
-        end_point += 1
+        end_decl += 1
 
         # If a semicolon is found before an open bracket,
         # this symbol is associated with a forward
         # declaration.
-        if view.substr(end_point) == ';':
-            return True, end_point
+        if view.substr(end_decl) == ';':
+            return True, end_decl - 1
 
-        if view.substr(end_point) == '{':
-            break
+        if view.substr(end_decl) == '{':
+            return False, end_decl - 1
+       
+def get_declaration(view, symbol_reg):
+    decl_start = get_decl_start(view, symbol_reg.begin())
+
+    is_fwd_decl, decl_end = get_decl_end(view, symbol_reg.end())
+
+    return is_fwd_decl, sublime.Region(decl_start, decl_end)
+
+def get_def_start(view, decl_end):
+    def_start = decl_end
+
+    while True:
+        if def_start == view.size() - 1:
+            print("Error: missing definition")
+            assert False
+
+        def_start += 1
+
+        if view.substr(def_start) == '{':
+            return def_start + 1
+
+def get_def_end(view, def_start):
+    def_end = def_start
 
     open_bracket_count = 1
     closed_bracket_count = 0
@@ -67,43 +80,46 @@ def get_scope_end(view, symbol_end):
     # Search for the closing bracket associated with the
     # first open bracket
     while open_bracket_count != closed_bracket_count:
-        if end_point == view.size() - 1:
+        if def_end == view.size() - 1:
             print("Error: missing closing bracket")
             assert False
 
-        end_point += 1
+        def_end += 1
 
-        if view.substr(end_point) == '{':
+        if view.substr(def_end) == '{':
             open_bracket_count += 1
-        elif view.substr(end_point) == '}':
+        elif view.substr(def_end) == '}':
             closed_bracket_count += 1
 
-    return False, end_point
+    return def_end
 
+def get_definition(view, declaration_reg):
+    def_start = get_def_start(view, declaration_reg.end())
 
-def get_scope_region(view, symbol_reg):
-    """
-    Returns the region of the scope associated
-    with the symbol
+    def_end = get_def_end(view, def_start)
 
-    Parameters:
-        view - sublime view
-        symbol_reg - sublime.Region of a symbol in view
-    """
-    symbol_start = symbol_reg.begin()
-    declaration_start = get_decl_start(view, symbol_start)
+    return sublime.Region(def_start, def_end)
 
-    symbol_end = symbol_reg.end() - 1
-    is_fwd_decl, end_point = get_scope_end(view, symbol_end)
+def get_scope(view, symbol_reg):
+    is_fwd_decl, declaration_reg = get_declaration(view, symbol_reg)
 
-    return sublime.Region(declaration_start, end_point + 1)
+    if is_fwd_decl:
+        return None
+    
+    definition_reg = get_definition(view, declaration_reg)
 
+    if view.substr(declaration_reg).split()[0] == 'class':
+        scope = Class(view, definition_reg, declaration_reg)
+    else:
+        scope = Function(view, definition_reg, declaration_reg)
+
+    return scope
 
 # For testing
 class TestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for pair in self.view.symbols():
-            declaration_region = pair[0]
-            scope_region = get_scope_region(self.view,
-                                            declaration_region)
-            print(self.view.substr(scope_region))
+            scope = get_scope(self.view, pair[0])
+            if scope:
+                print(scope.declaration)
+                print(scope.get_panel_options())
