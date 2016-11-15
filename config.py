cb16d8701afc7adf63c07f5cc232ee4b3bda16c6
Benny Ng <bennyng@umich.edu>
2016-11-14 19:06:01 -0500

    config.py is static now

--- config.py
+++ config.py
@@ -1,95 +1,88 @@
+import json
+import os
+import sublime
 
 
 class Config:
-    """
-    Config class to serve as a container and obejct for CodeReader
-        configurations
-    Formatted as a json file, but loaded as a dictioanry in memory
-    """
+    '''Config class to serve as a container and obejct for CodeReader
+    configurations
+    Formatted as a json file, but loaded as a dictioanry in memory'''
     DEFAULT_CONFIG = {
         'read_comments': False,
         'read_line_numbers': False,
 
     }
+    config = {}
+    package_dir = 'CodeReader'
+    config_fn = os.path.join(sublime.packages_path(),
+                             package_dir, '.cr_config')
 
-    DEFAULT_CONFIG_FN = '.cr_config'
-    config_fn = DEFAULT_CONFIG_FN
-
-    def __init__(self, fn=None, config={}):
-        """
-        Will initialize the Config object with a given config, if given
-        one. Otherwise it will default to just the default
+    @staticmethod
+    def init(fn=None, config={}):
+        '''
+        Will initialize the Config object with a given config, if
+        given one. Otherwise it will default to just the default
         configuration file.
         Will try to load the defaultly named config file if it exists.
         Otherwise, it will merely have the default configurations.
-        """
+        '''
 
+        # load the file from memory or default
         if not fn:
-            self.config = self.DEFAULT_CONFIG
+            Config.config = Config.DEFAULT_CONFIG
+            Config._save_config()
         else:
 
-            fn = os.path.join(os.path.dirname(__file__), fn)
-            self._load_config(fn)
-            self.config_fn = fn
+            Config.config_fn = os.path.join(sublime.packages_path(),
+                                            Config.package_dir, fn)
+            Config._load_config()
+            Config.config_fn = fn
 
+        # load param config file into our config
         for k in config:
-            self.config[k] = config[k]
+            Config.config[k] = config[k]
 
-    def get(self):
-        """
-        Returns the entire config file
-        """
-        return self.config
-
-    def get(self, param):
-        """
+    @staticmethod
+    def get(param):
+        '''
         Returns the value set for the given parameter in the config file.
         Will return None if the value is unset.
-        """
-        if param in self.config:
-            return self.config[param]
+        '''
+        if param in Config.config:
+            return Config.config[param]
         else:
             return None
 
-    def set(self, param, value):
-        """
-        Just sets the value for the parameter to the value, regardless
-        of whether or not it currently exists
-        Returns true if the action changes a previously set value
-        """
-        result = param in self.config
-        self.config[param] = value
-        self._save_config()
-        return result
+    @staticmethod
+    def set(param, value):
+        Config.config[param] = value
+        Config._save_config()
 
-    def _load_config(self):
-        """
-        loads config file into memory
-        parameters:
-            fn - filename of the config file
-        """
-
-        print('loading:', self.fn)
+    # loads config file into memory
+    #   @param: fn: filename of the config file
+    @staticmethod
+    def _load_config():
+        print('loading from:', Config.config_fn)
         try:
-            f = open(self.config_fn, 'r')
-            s = f.read()
-            # print(s)
-            self.config = json.loads(f.read())
+            f = open(Config.config_fn, 'r')
+            Config.config = json.loads(f.read())
         except:
             print("Error reading config file.")
 
-    def _save_config(self):
-        """
-        saves config file in memory to file
-        parameters:
-            fn - filename of the config file
-        """
-        print('saving to:', self.config_fn)
+    #  saves config file in memory to file
+    #   @param: fn: filename of the config file
+    @staticmethod
+    def _save_config():
+        print('saving to:', Config.config_fn)
         try:
-            f = open(self.config_fn, 'w')
-            f.write(json.dumps(self.config))
+            f = open(Config.config_fn, 'w')
+            f.write(json.dumps(Config.config))
         except:
             print("Error saving to config file. Write failed.")
 
-    def __str__(self):
-        return str(self.config)
+    @staticmethod
+    def __str__():
+        return str(Config.config)
+
+
+Config.init()
