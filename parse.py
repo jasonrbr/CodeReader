79b4ed50df5f4924a4450ff29ef0c65c9500aa55
Madeline Andrew <madclare@umich.edu>
2016-11-16 13:12:32 -0500

    update to pep8 standards

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 from .scopes import Function, Class
 
+
 def get_decl_start(view, symbol_start):
     """
     Returns the start point of the symbol's declaration
@@ -31,10 +32,11 @@ def get_decl_start(view, symbol_start):
 
     return start_decl
 
+
 def get_decl_end(view, symbol_end):
     end_decl = symbol_end
 
-     # Find first open bracket or ';'
+    # Find first open bracket or ';'
     while True:
         if end_decl == view.size() - 1:
             print("Error: missing open bracket or semicolon")
@@ -50,13 +52,15 @@ def get_decl_end(view, symbol_end):
 
         if view.substr(end_decl) == '{':
             return False, end_decl - 1
-       
+
+
 def get_declaration(view, symbol_reg):
     decl_start = get_decl_start(view, symbol_reg.begin())
 
     is_fwd_decl, decl_end = get_decl_end(view, symbol_reg.end())
 
     return is_fwd_decl, sublime.Region(decl_start, decl_end)
+
 
 def get_def_start(view, decl_end):
     def_start = decl_end
@@ -70,6 +74,7 @@ def get_def_start(view, decl_end):
 
         if view.substr(def_start) == '{':
             return def_start + 1
+
 
 def get_def_end(view, def_start):
     def_end = def_start
@@ -93,6 +98,7 @@ def get_def_end(view, def_start):
 
     return def_end
 
+
 def get_definition(view, declaration_reg):
     def_start = get_def_start(view, declaration_reg.end())
 
@@ -100,12 +106,13 @@ def get_definition(view, declaration_reg):
 
     return sublime.Region(def_start, def_end)
 
+
 def get_scope(view, symbol_reg):
     is_fwd_decl, declaration_reg = get_declaration(view, symbol_reg)
 
     if is_fwd_decl:
         return None
-    
+
     definition_reg = get_definition(view, declaration_reg)
 
     if view.substr(declaration_reg).split()[0] == 'class':
@@ -114,6 +121,7 @@ def get_scope(view, symbol_reg):
         scope = Function(view, definition_reg, declaration_reg)
 
     return scope
+
 
 # For testing
 class TestCommand(sublime_plugin.TextCommand):
