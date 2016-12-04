9f386b12e129da7cf073c3581cdb6a5f3714bd86
Benny Ng <bennyng@umich.edu>
2016-12-04 14:34:08 -0500

    Added windows hotkeys for speed+/-, added conditional check for Config init.

--- config.py
+++ config.py
@@ -71,6 +71,7 @@ class Config:
     #   @param: fn: filename of the config file
     @staticmethod
     def _load_config():
+        f = None
         try:
             f = open(Config.config_fn, 'r')
             Config.config = json.loads(f.read())
@@ -78,19 +79,28 @@ class Config:
             e = sys.exc_info()[0]
             print("Error reading config file ({}). Loading default.".format(e))
             Config.config = Config.DEFAULT_CONFIG
+        finally:
+            if f:
+                f.close()
+
+
 
     #  saves config file in memory to file
     #   @param: fn: filename of the config file
     @staticmethod
     def _save_config():
         print('saving to:', Config.config_fn)
+        f = None
         try:
-            f = open(Config.config_fn, 'w+')
+            f = open(Config.config_fn, 'w')
             f.write(json.dumps(Config.config))
         except:
             e = sys.exc_info()[0]
             print("Error saving to config file. Write failed.",
                   Config.config_fn, '({})'.format(e))
+        finally:
+            if f:
+                f.close()
 
     @staticmethod
     def toggle(param):
@@ -103,6 +113,9 @@ class Config:
 
     @staticmethod
     def increase_speed():
+        if not Config.is_initialized:
+            Config.init()
+
         speed = Config.get('speed')
         print('speed: ', speed)
         speed += 25
@@ -110,6 +123,9 @@ class Config:
 
     @staticmethod
     def decrease_speed():
+        if not Config.is_initialized:
+            Config.init()
+
         speed = Config.get('speed')
         speed -= 25
         if (speed < 100):
