546aeda9d2393b7488942c2e5a267e561edbd041
Benny Ng <bennyng@umich.edu>
2016-12-05 12:56:45 -0500

    basic templated container parsing

--- scopes.py
+++ scopes.py
@@ -21,11 +21,24 @@ symbol_list = {r'cout': 'see out', r'cin': 'see in', r'endl': 'endline',
                r'//': ' comment: ', r'/*': ' comment: ', r'*/': ' end comment: ',
                r'>=': ' greater than or equal to ',
                r'*': ' star ', r'&': ' ampersand ', r'(': ' ', r')': ' ',
-               r'|': ' bar ', ' < ': ' less than ', r' > ': ' greater than '}
+               r'|': ' bar ', ' < ': ' less than ', r' > ': ' greater than ',
+            }
 
 # Need to sort by descending length
 symbols = sorted(symbol_list.keys(), key=len, reverse=True)
 
+# temp function for dynamic templated container parsing
+# separate from symbol list for now to lower design complexity
+def temp_container_parsing(input_str):
+    pattern = re.compile('(\w+)\s?\<\s?(\w+)\>')
+    m = re.search(pattern, input_str)
+    original_match = m.group(0)
+    container_type = m.group(1)
+    template_type = m.group(2)
+
+    repl = '{} of type {}'.format(container_type, template_type)
+    parsed = re.sub(original_match, repl, input_str)
+    return parsed
 
 # Returns a stripped string with symbols mapped to their
 # appropriate English words
